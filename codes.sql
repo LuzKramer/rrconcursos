d32f0984cc96c76fe09aa37ac1e6fb78139ecf5a
William Kramer <kramerluz@gmail.com>
2023-08-05 17:19:21 -0400

    0.7 more pages in dashboard

--- codes.sql
+++ codes.sql
@@ -1,16 +1,43 @@
-CREATE TABLE questoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    pergunta TEXT NOT NULL,
-    banca VARCHAR(255) NOT NULL,
-    ano INT NOT NULL,
-    instituicao VARCHAR(255) NOT NULL,
-    materia VARCHAR(255) NOT NULL
+- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS db_rrconcursos;
+USE db_rrconcursos;
+
+-- Tabela Disciplinas
+CREATE TABLE IF NOT EXISTS disciplinas (
+  id_disciplina INT AUTO_INCREMENT PRIMARY KEY,
+  nome_disciplina VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE opcoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    questao_id INT NOT NULL,
-    descricao TEXT NOT NULL,
-    correta TINYINT NOT NULL,
-    FOREIGN KEY (questao_id) REFERENCES questoes(id)
+-- Tabela Instituicao
+CREATE TABLE IF NOT EXISTS instituicao (
+  id_instituicao INT AUTO_INCREMENT PRIMARY KEY,
+  nome_instituicao VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS bancas (
+  id_banca INT AUTO_INCREMENT PRIMARY KEY,
+  nome_banca VARCHAR(100) NOT NULL
+);
+
+-- Tabela Questoes
+CREATE TABLE IF NOT EXISTS questoes (
+  id_questao INT AUTO_INCREMENT PRIMARY KEY,
+  id_disciplina INT NOT NULL,
+  id_instituicao INT NOT NULL,
+  ano INT NOT NULL,
+  enunciado TEXT NOT NULL,
+  FOREIGN KEY (id_disciplina) REFERENCES disciplinas(id_disciplina) ON DELETE CASCADE,
+  FOREIGN KEY (id_instituicao) REFERENCES instituicao(id_instituicao) ON DELETE CASCADE
+);
+ CREATE TABLE IF NOT EXISTS alternativas (
+  id_alternativa INT AUTO_INCREMENT PRIMARY KEY,
+  id_questao INT NOT NULL,
+  txt_alt1 TEXT NOT NULL,
+  txt_alt2 TEXT NOT NULL,
+  txt_alt3 TEXT NOT NULL,
+  txt_alt4 TEXT NOT NULL,
+  txt_alt5 TEXT NOT NULL,
+  correta INT NOT NULL,
+  FOREIGN KEY (id_questao) REFERENCES questoes(id_questao) ON DELETE CASCADE
 );
