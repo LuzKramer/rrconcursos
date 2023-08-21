9dab71cac1e02410e3e3d8cd6b18f6568af1fd12
WKramer <kramerluz@gmail.com>
2023-08-21 11:35:01 -0400

    0.16

--- control/adms/news.php
+++ control/adms/news.php
@@ -1,5 +1,4 @@
 
-
 <?php
 // Database connection
 $servername = "localhost";
@@ -12,14 +11,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Delete
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $sql = "DELETE FROM tb_news WHERE id=?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $title = $_POST['title'];
     $news = $_POST['news'];
     $date = date("Y-m-d");  // Use Y-m-d format for MySQL date
-
-
-
 
     if (isset($_FILES['img'])) {
         $img = $_FILES['img'];
@@ -47,45 +52,24 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $save = $folder . $newname . "." . $extension;
 
         $bora = move_uploaded_file($img["tmp_name"], $path);
-
     }
-// Use prepared statements to prevent SQL injection
-$sql = "INSERT INTO tb_news (title, news, date, img) VALUES (?, ?, ?, ?)";
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "ssss", $title, $news, $date, $save);
 
-if (mysqli_stmt_execute($stmt)) {
-    echo "<script>alert('NOticia adicionada com sucesso!');</script>";
-} else {
-    echo "<script>alert('erroao adicionar noticia!');</script>". mysqli_error($conn);
+    // Use prepared statements to prevent SQL injection
+    $sql = "INSERT INTO tb_news (title, news, date, img) VALUES (?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ssss", $title, $news, $date, $save);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "<script>alert('Noticia adicionada com sucesso!');</script>";
+    } else {
+        echo "<script>alert('Erro ao adicionar noticia!');</script>". mysqli_error($conn);
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 }
-
-
-// Update
-if (isset($_POST['update'])) {
-    $id = $_POST['id'];
-    $title = $_POST['title'];
-    $news = $_POST['news'];
-    $date = $_POST['date'];
-    $img = $_POST['img'];
-
-    $sql = "UPDATE tb_news SET title='$title', news='$news', date='$date', img='$img' WHERE id=$id";
-    $conn->query($sql);
-}
-
-// Delete
-if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
-    $sql = "DELETE FROM tb_news WHERE id=$id";
-    $conn->query($sql);
-}
-
-
-mysqli_stmt_close($stmt);
-mysqli_close($conn);
-}
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -116,36 +100,36 @@ mysqli_close($conn);
             </div>
             <button type="submit" name="create" class="btn btn-primary">Create</button>
         </form>
-<!-- Read data -->
-<h2 class="mt-4">News List</h2>
-<table class="table">
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        // Move this section outside the POST request block
-        $sql = "SELECT * FROM tb_news";
-        $result = $conn->query($sql);
 
-        while ($row = $result->fetch_assoc()):
-        ?>
-        <tr>
-            <td><?php echo $row['title']; ?></td>
-            <td><?php echo $row['date']; ?></td>
-            <td>
-                <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-info btn-sm">Edit</a>
-                <a href="?delete=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
-            </td>
-        </tr>
-        <?php endwhile; ?>
-    </tbody>
-</table>
+        <!-- Read data -->
+        <h2 class="mt-4">News List</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Move this section outside the POST request block
+                $sql = "SELECT * FROM tb_news";
+                $result = $conn->query($sql);
 
+                while ($row = $result->fetch_assoc()):
+                ?>
+                <tr>
+                    <td><?php echo $row['title']; ?></td>
+                    <td><?php echo $row['date']; ?></td>
+                    <td>
+                        <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-info btn-sm">Edit</a>
+                        <a href="?delete=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </div>
 
     <!-- Include Bootstrap JS -->
