6fa0a31378ddeef382edca5a5188cd7ddc691ac1
computer veio ifrr <kramerluz@gmail.com>
2023-09-21 09:31:35 -0400

    0.26.5

--- control/infos.php
+++ control/infos.php
@@ -5,6 +5,14 @@
     <meta charset="UTF-8">
     <title>rrCONCURSOS/</title>
     <link rel="stylesheet" href="../view/style.css">
+    <style>
+        .info {
+            background-color: lightgray;
+            padding: 20px;
+            border: 1px solid gray;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 
 <body>
@@ -39,7 +47,207 @@
 
         </div>
 
-    </header>
-</body>
 
+    </header>
+    
+    <div class="info">
+        <h2>Informações sobre o RR CONCURSOS</h2>
+        <p> 
+
+
+        <html>
+<head>
+    <title>RR CONCURSOS - O seu caminho para a aprovação</title>
+    <style>
+        body {
+            
+            margin: 0;
+            padding: 20px;
+        }
+        
+        h1 {
+            color: ;
+            font-size: 32px;
+            margin-bottom: 20px;
+        }
+        
+        p {
+            font-size: 18px;
+            line-height: 1.5;
+            margin-bottom: 10px;
+        }
+        
+        .highlight {
+            background-color: yellow;
+            padding: 5px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h1>RR CONCURSOS - O seu caminho para a aprovação, meu chapa!</h1>
+    
+    <p>E aí, beleza? Se liga nesse site massa chamado RR CONCURSOS, o lugar certo pra você se preparar pra esses vestibular topzera, manja?</p>
+    
+    <p>com as melhores dicas, material de estudo e tudo que ce precisa pra mandar bem nesses vestibular, sacou?</p>
+    
+    <p></p>
+    
+    <p>Aqui na RR CONCURSOS, acreditamos que com dedicação, disciplina e acesso a conteúdo de qualidade, qualquer um pode chegar lá, tá ligado? Por isso, a gente tá sempre junto contigo pra te dar todo o suporte necessário pra tu mandar bem e conquistar a tua aprovação, meu parceiro!</p>
+    
+    <p>Então, bora mudar de vida e correr atrás daquela carreira dos sonhos, meu chapa! Vem com a gente e se junta a milhares de estudantes que tão confiando na RR CONCURSOS pra trilhar o caminho da aprovação, beleza?</p>
+    
+    <p class="highlight">Tamo junto pra te ajudar a alcançar o sucesso, meu brother! Pode contar com a RR CONCURSOS, tá ligado?</p>
+</body>
 </html>
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </p>
+        <p></p>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
